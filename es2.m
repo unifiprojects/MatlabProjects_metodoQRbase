69e0461baf5b7746004074a49154f4791a965517
Matteo Mauro <mauro.matteo96@gmail.com>
2019-12-05 18:16:24 +0100

    es2 autovalori complessi coniugati

---
+++ es2.m
@@ -0,0 +1,13 @@
+clc
+clear
+B = [8 -1 -5
+     -4 4 -2
+     18 -5 7];
+
+[T, Q, R] = metodoQRbase(B, 10000);
+T
+autoval_complessi = eig(T(1:2, 1:2))
+schur = schur(B)
+autovalori = eig(B)
+% la matrice T assume una forma triangolare a blocchi in quanto esistono 2
+% autovalori complessi coniugati
