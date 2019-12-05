1083edab92190866c92a76b2c21313e94ee65d77
Matteo Mauro <mauro.matteo96@gmail.com>
2019-12-05 18:20:24 +0100

    es3

---
+++ es3.m
@@ -0,0 +1,14 @@
+clc
+clear
+C = [17 24 1 8 15
+     23 5 7 14 16
+     4 6 13 20 22
+     10 12 19 21 3
+     11 18 25 2 9];
+ [T, Q, R] = metodoQRbase(C, 10000);
+ T
+ schur = schur(C)
+ autovalori = eig(C)
+ % la T non converge correttamente con l'implementazione base del metodo
+ % QR, mentre la forma di schur di matlab calcola correttamente gli
+ % autovalori
